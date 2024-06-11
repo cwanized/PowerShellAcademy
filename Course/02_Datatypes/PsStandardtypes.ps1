0d7a6ea7b739f12add18cbb936027082093b7642
Walther Christopher, B2B-ITL-PWU-CVM-WHM <Christopher.Walther@swisscom.com>
2024-06-11 10:43:43 +0200

    chore: Refactor PsStandardtypes.ps1 for better code organization and readability

--- Course/02_Datatypes/PsStandardtypes.ps1
+++ Course/02_Datatypes/PsStandardtypes.ps1
@@ -9,3 +9,50 @@
 
 [datetime]"12.01.2024"
 [datetime]"12.13.2024"
+
+
+#you could
+
+
+function Do-Something($File){
+    $File
+
+}
+
+
+
+
+#but you should
+function Do-Somethingbetter{
+    param(
+        [System.IO.FileInfo]$File,
+        $date
+
+    )
+    $path
+    $date
+
+}
+
+
+
+
+function Do-SomethingbetterV2{
+    param(
+        [ValidateScript({
+            if( -Not ($_ | Test-Path) ){
+            write-host "noFile"
+                throw "File or folder does not exist"
+            }else{
+                $false
+            }
+            
+        })]
+        [System.IO.FileInfo]$Path,
+        $date
+
+    )
+    $path
+    $date
+
+}
